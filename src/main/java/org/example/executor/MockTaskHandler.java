ac7c3f8c307e2a8ba73ed66a8081a469714882ff
i37883 <aravind.nair@verisk.com>
2023-08-17 13:20:17 +0530

    Added default implemenation

--- src/main/java/org/example/executor/MockTaskHandler.java
+++ src/main/java/org/example/executor/MockTaskHandler.java
@@ -25,49 +25,53 @@ public class MockTaskHandler implements TaskHandler{
     @Value("${app.mockExecutor.causeRandomTaskFailureProbability}")
     private Float taskFailureProbability ;
 
-
-
     @Override
     public void handleTaskExecution(AsyncTask task) {
         log.debug("EXEC -> handleTaskExecution()");
         Thread thread = new Thread(() -> {
-            try {
-                task.processingTime = random.nextLong(processingTimeLowerLimit, processingTimeUpperLimit);
-                task.taskStatus = TaskStatus.INITIATED.getValue();
-                log.info(">>> "+ task.taskId +" Initiated task - ETA : "+task.processingTime);
-                if(task.awaitTaskCompletion){
-                    if(getTaskFail()){
-                        throw new InterruptedException();
-                    }
-                    Thread.sleep(task.processingTime);
-                    if(task.taskStatus.equalsIgnoreCase(TaskStatus.INITIATED.getValue()))
-                        task.taskStatus=TaskStatus.COMPLETED.getValue();
-                    doCallBack(task);
-                }
-                else{
-                    doCallBack(task);
-                    Thread.sleep(task.processingTime);
-                }
-
-                log.info("### "+ task.taskId +" Done executing in " + Double.valueOf(task.processingTime)/1000+" secs");
-                task.executor.taskMonitor.release(task);
-            } catch (InterruptedException e) {
-                task.taskStatus=TaskStatus.FAILED.getValue();
-                doCallBack(task);
-                log.info("### "+ task.taskId +" Done executing with failure");
-                task.executor.taskMonitor.release(task);
-            }
+            executeTask(task);
         });
         thread.start();
     }
 
-    private void doCallBack(AsyncTask task){
+    @Override
+    public void executeTask(AsyncTask task) {
+        try {
+            task.processingTime = random.nextLong(processingTimeLowerLimit, processingTimeUpperLimit);
+            task.taskStatus = TaskStatus.INITIATED.getValue();
+            log.info(">>> "+ task.taskId +" Initiated task - ETA : "+task.processingTime);
+            if(task.awaitTaskCompletion){
+                if(shouldTaskFail()){
+                    throw new InterruptedException();
+                }
+                Thread.sleep(task.processingTime);
+                if(task.taskStatus.equalsIgnoreCase(TaskStatus.INITIATED.getValue()))
+                    task.taskStatus=TaskStatus.COMPLETED.getValue();
+                doCallBack(task);
+            }
+            else{
+                doCallBack(task);
+                Thread.sleep(task.processingTime);
+            }
+
+            log.info("### "+ task.taskId +" Done executing in " + Double.valueOf(task.processingTime)/1000+" secs");
+            task.executor.taskMonitor.release(task);
+        } catch (InterruptedException e) {
+            task.taskStatus=TaskStatus.FAILED.getValue();
+            doCallBack(task);
+            log.info("### "+ task.taskId +" Done executing with failure");
+            task.executor.taskMonitor.release(task);
+        }
+    }
+
+    @Override
+    public void doCallBack(AsyncTask task){
         task.executor.callBack(task);
         task.latch.countDown();
         log.debug("==="+ task.taskId +" Callback called");
     }
 
-    private Boolean getTaskFail(){
+    private Boolean shouldTaskFail(){
         if(causeRandomFailures){
             if(taskFailureProbability>random.nextFloat(0,1))
                 return Boolean.TRUE;
--- src/main/java/org/example/executor/TaskHandler.java
+++ src/main/java/org/example/executor/TaskHandler.java
@@ -1,5 +1,14 @@
 package org.example.executor;
 
 public interface TaskHandler {
-    public void handleTaskExecution(AsyncTask task);
+    default void handleTaskExecution(AsyncTask task) {
+        Thread thread = new Thread(() -> executeTask(task));
+        thread.start();
+    }
+    void executeTask(AsyncTask task);
+
+    default void doCallBack(AsyncTask task) {
+        task.executor.callBack(task);
+        task.latch.countDown();
+    }
 }
